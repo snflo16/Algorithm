194ceeaadb21a714168d6d759c38063d67bb337a
Hyeeun <snflo16@naver.com>
2022-06-07 22:43:19 +0900

    0510|algo

---
+++ 0510/11057.py
@@ -0,0 +1,12 @@
+N = int(input())
+
+dp = [[0]*10 for _ in range(N)]
+for i in range(10):
+    dp[0][i] = 1
+
+for i in range(1, N):
+    for j in range(10):
+        for k in range(j+1):
+            dp[i][j] += dp[i-1][k]
+
+print(sum(dp[N-1])%10007)
